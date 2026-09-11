49175998a8f0b98daa19e4beea6a69f4a2343037
mihikagupta9876 <mihikagupta9542@gmail.com>
2026-09-11 23:02:09 +0530

    Update db.js

--- Task-2-ATTENDX/server/src/db.js
+++ Task-2-ATTENDX/server/src/db.js
@@ -68,7 +68,14 @@ if (!count) {
   const now = new Date().toISOString();
   const pass = bcrypt.hashSync('admin123',10);
   db.prepare('INSERT INTO users VALUES (?,?,?,?,?,?)').run('org-demo','Mihika Gupta','organizer@attendx.local',pass,'organizer',now); const p2 = bcrypt.hashSync('student123',10);
-  db.prepare('INSERT INTO users VALUES (?,?,?,?,?,?,?)').run('student-demo','Demo Student','student@attendx.local',p2,'attendee',now);
+ db.prepare('INSERT INTO users VALUES (?,?,?,?,?,?)').run(
+  'student-demo',
+  'Demo Student',
+  'student@attendx.local',
+  p2,
+  'attendee',
+  now
+);
   const id = randomUUID();
 
   const start = new Date(
